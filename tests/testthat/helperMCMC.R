e7bf9d504509bc74eab474ed50cab646ff604436
Evan A Martin <Evatar@Syl.local>
2018-09-21 08:49:31 -0700

    Include tests for the MHEdge function

--- tests/testthat/helperMCMC.R
+++ tests/testthat/helperMCMC.R
@@ -1,34 +1,5 @@
 ################################################################################
-# Generate data under topology M1 with only gene expression nodes.
-# T1 -> T2 -> T3
-################################################################################
-
-m1ge <- function (N, ss) {
-
-  T1 <- rNoParents(N = N,
-                   b0 = 0,
-                   s = 1)
-
-  T2 <- rMParents(N = N,
-                  mParents = 1,
-                  parentData = list(T1),
-                  b0 = 0,
-                  b1 = list(ss),
-                  s = 1)
-
-  T3 <- rMParents(N = N,
-                  mParents = 1,
-                  parentData = list(T2),
-                  b0 = 0,
-                  b1 = list(ss),
-                  s = 1)
-
-  return (cbind(T1, T2, T3))
-
-}
-
-################################################################################
-# Generate data under topology H2
+# Generate data under topology H2:
 # T1 -> T2 -> T4 -> T5 <- T3 <- T1
 ################################################################################
 
@@ -67,5 +38,34 @@ h2 <- function (N, ss) {
                   s = 1)
 
   return (cbind(T1, T2, T3, T4, T5))
+
+}
+
+################################################################################
+# Generate data under topology M1 with only gene expression nodes:
+# T1 -> T2 -> T3
+################################################################################
+
+m1ge <- function (N, ss) {
+
+  T1 <- rNoParents(N = N,
+                   b0 = 0,
+                   s = 1)
+
+  T2 <- rMParents(N = N,
+                  mParents = 1,
+                  parentData = list(T1),
+                  b0 = 0,
+                  b1 = list(ss),
+                  s = 1)
+
+  T3 <- rMParents(N = N,
+                  mParents = 1,
+                  parentData = list(T2),
+                  b0 = 0,
+                  b1 = list(ss),
+                  s = 1)
+
+  return (cbind(T1, T2, T3))
 
 }
