552e6eadcaf33ab6b338a48d50775145a4d935a8
Evan A Martin <Evatar@Syl.local>
2018-10-04 10:26:35 -0700

    Eliminate redundant tests.

--- tests/testthat/testCycles.R
+++ tests/testthat/testCycles.R
@@ -1,6 +1,6 @@
 context('Identify cycles')
 
-test_that('cyclePrep returns the correct number of cycles',{
+test_that('cyclePrep returns the correct cycles',{
 
   # The following graph has three cycles two of which are nested in the largest
   # cycle.
@@ -18,26 +18,13 @@ test_that('cyclePrep returns the correct number of cycles',{
 
   cp <- cyclePrep(adjMatrix)
 
-  expect_equal(length(cp$dnUnique), 6)
-  expect_equal(length(cp$edgeNum), 6)
-  expect_equal(length(cp$wCycle), 6)
-
-  # The following adjacency matrix comes from the data from the pcalg package
-  adjMatrix <- matrix(c(0, 1, 0, 0, 0, 1, 0, 1,
-                        0, 0, 1, 0, 1, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 1, 0, 1,
-                        0, 0, 0, 0, 0, 0, 1, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0),
-                      byrow = TRUE,
-                      nrow = 8)
-
-  cp <- cyclePrep(adjMatrix)
-
-  expect_equal(length(cp$dnUnique), 6)
-  expect_equal(length(cp$edgeNum), 6)
+  expect_identical(cp$dnUnique, c(91, 3269, 16, 3191, 50, 53))
+  expect_equal(cp$edgeNum, list(c(2, 4, 1),
+                                c(3, 7, 6, 5, 4, 1),
+                                c(1, 4, 2),
+                                c(3, 7, 6, 5, 2),
+                                c(2, 5, 6, 7, 3),
+                                c(1, 4, 5, 6, 7, 3)))
   expect_equal(length(cp$wCycle), 6)
 
   # The following adjacency matrix is from topology H2
@@ -51,8 +38,9 @@ test_that('cyclePrep returns the correct number of cycles',{
 
   cp <- cyclePrep(adjMatrix)
 
-  expect_equal(length(cp$dnUnique), 2)
-  expect_equal(length(cp$edgeNum), 2)
+  expect_identical(cp$dnUnique, c(288, 105))
+  expect_equal(cp$edgeNum, list(c(2, 4, 5, 3, 1),
+                                c(1, 3, 5, 4, 2)))
   expect_equal(length(cp$wCycle), 2)
 })
 
@@ -95,40 +83,5 @@ test_that('cyclePrep returns NULL if there are no cycles in the graph', {
   expect_true(is.null(cp$dnUnique))
   expect_true(is.null(cp$edgeNum))
   expect_true(is.null(cp$wCycle))
-
-})
-
-test_that('cyclePrep returns the correct decimal for each cycle', {
-
-  # The following adjacency matrix is from topology H2
-  adjMatrix <- matrix(c(0, 1, 1, 0, 0,
-                        0, 0, 0, 1, 0,
-                        0, 0, 0, 0, 1,
-                        0, 0, 0, 0, 1,
-                        0, 0, 0, 0, 0),
-                      byrow = TRUE,
-                      nrow = 5)
-
-  cp <- cyclePrep(adjMatrix)
-
-  expect_identical(cp$dnUnique, c(288, 105))
-
-  # The following graph has three cycles two of which are nested in the largest
-  # cycle.
-  # T1 - T2 - T3 - T1
-  # T1 - T3 - T4 - T5 - T6 - T1
-  # T1 - T2 - T3 - T4 - T5 - T6 - T1
-  adjMatrix <- matrix(c(0, 1, 1, 0, 0, 1,
-                        1, 0, 1, 0, 0, 0,
-                        1, 1, 0, 1, 0, 0,
-                        0, 0, 1, 0, 1, 0,
-                        0, 0, 0, 1, 0, 1,
-                        1, 0, 0, 0, 1, 0),
-                      byrow = TRUE,
-                      nrow = 6)
-
-  cp <- cyclePrep(adjMatrix)
-
-  expect_identical(cp$dnUnique, c(91, 3269, 16, 3191, 50, 53))
 
 })
