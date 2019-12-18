661813145061510ee50edc400ea12b37b2c37287
Evan A Martin <Evatar@fi.local>
2019-12-18 15:55:10 -0800

    Fix bug in the MSE tests.

--- tests/testthat/testMCMC.R
+++ tests/testthat/testMCMC.R
@@ -2,38 +2,25 @@ context('baycn without genetic variants')
 
 test_that('the mhEdge function infers the correct graph',{
 
-  # Adjacency matrix for M1 - M4 -----------------------------------------------
+  # Adjacency matrix for M1 and M2 ---------------------------------------------
 
-  # Fully connected adjacency matrix for M1 - M4
-  am_m <- matrix(c(0, 1, 1,
-                   0, 0, 1,
-                   0, 0, 0),
-                 byrow = TRUE,
-                 nrow = 3)
+  # Adjacency matrix for M1
+  am_m1 <- matrix(c(0, 1, 0,
+                    0, 0, 1,
+                    0, 0, 0),
+                  byrow = TRUE,
+                  nrow = 3)
 
-  # Run baycn on data simulated for M2 -----------------------------------------
+  # Adjacency matrix for M2
+  am_m2 <- matrix(c(0, 1, 0,
+                    0, 0, 0,
+                    0, 1, 0),
+                  byrow = TRUE,
+                  nrow = 3)
+
+  # Simulate data for M1 and M2 ------------------------------------------------
 
   set.seed(5)
-
-  data_m2 <- simdata(b0 = 0,
-                     N = 500,
-                     s = 1,
-                     ss = 1,
-                     graph = 'm2_ge')
-
-  baycn_m2 <- mhEdge(adjMatrix = am_m,
-                     burnIn = 0.2,
-                     data = data_m2,
-                     iterations = 1000,
-                     nGV = 0,
-                     pmr = FALSE,
-                     prior = c(0.05,
-                               0.05,
-                               0.9),
-                     progress = FALSE,
-                     thinTo = 200)
-
-  # Run baycn on data simulated for M1 -------------------------------------------
 
   data_m1 <- simdata(b0 = 0,
                      N = 500,
@@ -41,7 +28,15 @@ test_that('the mhEdge function infers the correct graph',{
                      ss = 1,
                      graph = 'm1_ge')
 
-  baycn_m1 <- mhEdge(adjMatrix = am_m,
+  data_m2 <- simdata(b0 = 0,
+                     N = 500,
+                     s = 1,
+                     ss = 1,
+                     graph = 'm2_ge')
+
+  # Run baycn on M1 and M2 -----------------------------------------------------
+
+  baycn_m1 <- mhEdge(adjMatrix = am_m1,
                      burnIn = 0.2,
                      data = data_m1,
                      iterations = 1000,
@@ -53,27 +48,41 @@ test_that('the mhEdge function infers the correct graph',{
                      progress = TRUE,
                      thinTo = 200)
 
+  baycn_m2 <- mhEdge(adjMatrix = am_m2,
+                     burnIn = 0.2,
+                     data = data_m2,
+                     iterations = 1000,
+                     nGV = 0,
+                     pmr = FALSE,
+                     prior = c(0.05,
+                               0.05,
+                               0.9),
+                     progress = FALSE,
+                     thinTo = 200)
+
   # Calculate the MSE for M1 and M2 --------------------------------------------
-
-  # Expected probabilities for M2
-  ep_m2 <- matrix(c(1, 0, 0,
-                    0, 0, 1,
-                    0, 1, 0),
-                  byrow = TRUE,
-                  nrow = 3)
-
-  mse_m2 <- sum((baycn_m2@posteriorES[, 2:4] - ep_m2)^2)
 
   # Expected probabilities for M1
   ep_m1 <- matrix(c(0.33, 0.66, 0,
-                    0,    0,    1,
                     0.66, 0.33, 0),
                   byrow = TRUE,
-                  nrow = 3)
+                  nrow = 2)
 
   mse_m1 <- sum((baycn_m1@posteriorES[, 2:4] - ep_m1)^2)
 
-  expect_true(mse_m2 < 0.1)
+  # Expected probabilities for M2
+  ep_m2 <- matrix(c(1, 0, 0,
+                    0, 1, 0),
+                  byrow = TRUE,
+                  nrow = 2)
+
+  mse_m2 <- sum((baycn_m2@posteriorES[, 2:4] - ep_m2)^2)
+
+  # Print the MSEs for M1 and M2
+  print(mse_m1)
+  print(mse_m2)
+
   expect_true(mse_m1 < 0.1)
+  expect_true(mse_m2 < 0.1)
 
 })
--- tests/testthat/testMCMCGV.R
+++ tests/testthat/testMCMCGV.R
@@ -11,15 +11,23 @@ test_that('the mhEdge function infers the correct graph using PMR',{
                  byrow = TRUE,
                  nrow = 3)
 
-  # Run baycn on data simulated for M1 with GV ---------------------------------
+  # Simulate data for M1 and M3 ------------------------------------------------
 
-  set.seed(1)
+  set.seed(3)
 
   data_m1 <- simdata(b0 = 0,
                      N = 500,
                      s = 1,
                      ss = 1,
                      graph = 'm1_gv')
+
+  data_m3 <- simdata(b0 = 0,
+                     N = 500,
+                     s = 1,
+                     ss = 1,
+                     graph = 'm3_gv')
+
+  # Run baycn on M1 and M3 -----------------------------------------------------
 
   baycn_m1 <- mhEdge(adjMatrix = am_m,
                      burnIn = 0.2,
@@ -31,15 +39,7 @@ test_that('the mhEdge function infers the correct graph using PMR',{
                                0.05,
                                0.9),
                      progress = FALSE,
-                     thinTo = 200)
-
-  # Run baycn on data simulated for M3 with GV ---------------------------------
-
-  data_m3 <- simdata(b0 = 0,
-                     N = 500,
-                     s = 1,
-                     ss = 1,
-                     graph = 'm3_gv')
+                     thinTo = 500)
 
   baycn_m3 <- mhEdge(adjMatrix = am_m,
                      burnIn = 0.2,
@@ -51,10 +51,11 @@ test_that('the mhEdge function infers the correct graph using PMR',{
                                0.05,
                                0.9),
                      progress = TRUE,
-                     thinTo = 200)
+                     thinTo = 500)
 
   # Calculate the MSE for M1 and M3 --------------------------------------------
 
+  # Expected probabilities for M1 when using PMR
   ep_m1 <- matrix(c(1, 0, 0,
                     0, 0, 1,
                     1, 0, 0),
@@ -63,6 +64,7 @@ test_that('the mhEdge function infers the correct graph using PMR',{
 
   mse_m1 <- sum((baycn_m1@posteriorES[, 2:4] - ep_m1)^2)
 
+  # Expected probabilities for M3 when using PMR
   ep_m3 <- matrix(c(1, 0, 0,
                     1, 0, 0,
                     0, 0, 1),
@@ -70,6 +72,10 @@ test_that('the mhEdge function infers the correct graph using PMR',{
                   nrow = 3)
 
   mse_m3 <- sum((baycn_m3@posteriorES[, 2:4] - ep_m3)^2)
+
+  # Print the MSEs for M1 and M3
+  print(mse_m1)
+  print(mse_m3)
 
   expect_true(mse_m1 < 0.1)
   expect_true(mse_m3 < 0.1)
